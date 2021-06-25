3449e719e223152226c3aeac35764cb4ec1e23c1
Julio Alves <juliodeveloper0@gmail.com>
2021-06-25 02:05:02 -0300

    Editando o README.md, Adicinando as Screenshots e terminando o menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,31 @@
 const buttonMenuMobile = document.querySelector(".header .menu-hamburger");
 const menuMobile = document.querySelector(".header .nav-mobile");
 
-let menuOpen = false;
+buttonMenuMobile.addEventListener("click", handleClick);
 
-buttonMenuMobile.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuMobile.style.display = "block";
-    menuMobile.classList.remove("menu-close");
-    menuMobile.classList.add("menu-open");
-    menuOpen = true;
-  } else {
-    menuMobile.classList.remove("menu-open");
-    menuMobile.classList.add("menu-close");
+let htmladdEvent = false;
 
-    setTimeout(() => {
-      menuMobile.style.display = "none";
-    }, 300);
-    menuOpen = false;
+function handleClick() {
+  menuMobile.classList.toggle("active");
+  outsideClick(this);
+}
+
+function outsideClick(element) {
+  const html = document.documentElement;
+  if (!htmladdEvent) {
+    html.addEventListener("click", handleOutsideClick);
+    htmladdEvent = true;
   }
-});
+
+  function handleOutsideClick(event) {
+    const containsElement = element.parentNode.contains(event.target);
+    if (!containsElement) {
+      menuMobile.classList.remove("active");
+      html.removeEventListener("click", handleOutsideClick);
+      htmladdEvent = false;
+    }
+  }
+}
 
 const tagsA = document.querySelectorAll("* a");
 tagsA.forEach((e) => {
